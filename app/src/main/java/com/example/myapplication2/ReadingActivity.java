2a4c94abb85627355b04b00265362d35b6f2a124
daydayup113 <1173998698@qq.com>
2025-09-03 15:24:55 +0800

    删除饭也是的章节弹窗

--- app/src/main/java/com/example/myapplication2/ReadingActivity.java
+++ app/src/main/java/com/example/myapplication2/ReadingActivity.java
@@ -326,8 +326,6 @@ public class ReadingActivity extends AppCompatActivity {
                         Log.d(TAG, "loadPageContent: Scrolling to top");
                         contentScrollView.scrollTo(0, 0);
                     }
-                    
-                    Toast.makeText(ReadingActivity.this, "第 " + (currentPage + 1) + " 章", Toast.LENGTH_SHORT).show();
                 });
             } catch (OutOfMemoryError e) {
                 Log.e(TAG, "内存不足，无法加载章节", e);
